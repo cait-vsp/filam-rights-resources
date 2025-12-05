b402c787920ebb4c86687141be68a81818952ed6
Caitlin S. <caitlinsanpedro@gmail.com>
2025-12-05 11:18:09 -0800

    Added Background Image w/ Parallax Effect

---
+++ src/data/resources.js
@@ -0,0 +1,211 @@
+
+// Resources.js file is meant to store researched hard-coded data of organizations and resources for different catgeories.
+
+// Total Categories: Civic, Housing, Labor, Immigration, Health, Education, Culture
+
+export const resources = [
+    // CIVIC RESOURCES
+    {
+    id: 1,
+    title: "FilipinX Civic Engagement and Mental Health Resources",
+    org: "LEAD FILIPINO",
+    blurb: "A FilipinX nonprofit organization dedicated to promoting local engagement, education, and social awareness in the FilipinX American community.",
+    categories: ["Civic", "Health"],
+    location: ["San Jose, CA", "Northern California"],
+    language: ["English"],
+    link: "https://leadfilipino.org/"
+    },
+    {
+    id: 2,
+    title: "FilipinX Civic Engagement for Mental Health and Education",
+    org: "SIPA: Search to Involve Pilipino Americans",
+    blurb: "A PilipinX American nonprofit organization that empowers PilipinX Americans and others by providing health and human services, community economic development, arts and culture, and provides a place for all to join and strengthen the community.",
+    categories: ["Civic", "Health", "Education"],
+    location: ["Los Angeles, CA", "Southern California"],
+    language: ["English", "Tagalog"],
+    link: "https://www.sipacares.org/"
+    },
+    {
+    id: 3,
+    title: "San Francisco: FilipinX Community Center Serving ALL FilipinX Age Groups",
+    org: "Filipino Community Center (FCC)",
+    blurb: "A FilipinX nonprofit organization in San Francisco that is dedicated to advocating and providing services to the FilipinX community.",
+    categories: ["Civic", "Education", "Labor", "Immigration"],
+    location: ["San Francisco, CA", "Northern California"],
+    language: ["English", "Tagalog"],
+    link: "https://www.filipinocc.org/"
+    },
+    {
+    id: 4,
+    title: "FilipinX Youth & Student Org. Activism in US & PH",
+    org: "Kabataan Alliance",
+    blurb: "A FilipinX youth and student nonprofit organization dedicated to promoting justice for human rights and resources that ties to the US and the Philippines.",
+    categories: ["Civic", "Labor", "Immigration"],
+    location: ["Southern California", "Northern California"],
+    language: ["English", "Tagalog"],
+    link: "https://www.kabataanalliance.com/"
+    },
+    {
+    id: 5,
+    title: "NorCal FilipinX Americans in Civic Engagement",
+    org: "Filipino Americans of Northern California Organization (FANCO)",
+    blurb: "A FilipinX association in NorCal that highlights Filipino identity and culture through civic involvement and community activities.",
+    categories: ["Civic"],
+    location: ["Alameda", "Butte", "Chico", "Contra Costa", "Davis", "Dixon", "Escalon", "Fairfield", "Lathrop", "Lodi", "Manteca", "Marin", "Monterey", "Mountain House", "Oroville", "Petaluma", "Rio Vista", "Ripon", "Sacramento", "Santa Clara", "Santa Cruz", "San Francisco", "San Joaquin", "San Mateo", "Santa Rosa", "Siskiyou", "Solano", "Sonoma", "Stockton", "Tracy", "Vallejo", "Windsor", "Winters", "Woodland", "Yolo", "Yreka", "Northern California"],
+    language: ["English"],
+    link: "http://www.fanconorthbay.org/"
+    },
+    {
+    id: 6,
+    title: "FilipinX Community of Solano County Empowering FilipinX People",
+    org: "Filipino Community of Solano County",
+    blurb: "A FilipinX American nonprofit dedicated to educational, cultural, and humanitarian projects that empower FilipinX and immigrant families in Solano County.",
+    categories: ["Civic", "Education", "Health", "Culture"],
+    location: ["Vallejo", "Northern California"],
+    language: ["English"],
+    link: "https://filipinocommunityofsolanocounty.com/"
+    },
+
+    /*
+    {
+    id: 7,
+    title: " ",
+    org: " ",
+    blurb: " .",
+    categories: ["Civic"],
+    location: [" "],
+    language: ["English"],
+    link: " "
+    }*/
+
+
+    // HOUSING RESOURCES
+    {
+    id: 6,
+    title: "Bay Area Filipino American Real Estate Professionals",
+    org: "Filipino American Real Estate Professionals Association Bay Area, Inc.",
+    blurb: "A Filipino American nonprofit organization dedicated to promoting the interests of Filipino American professionals by elevating professionalism and unifying the real estate community in the Bay Area.",
+    categories: ["Civic", "Education", "Health", "Culture"],
+    location: ["San Francisco", "Northern California"],
+    language: ["English"],
+    link: "https://farepabayarea.org/"
+    },
+    
+
+
+
+    // LABOR RESOURCES
+    {
+    id: 4,
+    title: "Filipino Civic Engagement and Empowerment for Filipino Labor Workers",
+    org: "Pilipino Workers Center (PWC)",
+    blurb: "Aims to secure the dignity and safety of the PilipinX community in Southern California and build labor leaders in the domestic worker industry through power, leadership, and service.",
+    categories: ["Labor", "Civic", "Immigration"],
+    location: ["Southern California"],
+    language: ["English", "Tagalog"],
+    link: "https://www.pwcsc.org/"
+    },
+    {
+    id: 4,
+    title: "Empowerment of Filipino American Architects and Engineers of SoCal",
+    org: "Fil-Am Society of Architects and Engineers of Southern California (FASAE-SOCAL)",
+    blurb: "An association meant to represent FilipinX American architects and engineers through networks, technical and business practices, and professional growth.",
+    categories: ["Labor", "Civic"],
+    location: ["Carson", "Southern California"],
+    language: ["English"],
+    link: "https://www.fasae-socal.org/"
+    },
+    {
+    id: 4,
+    title: "Filipino American Fashion Artists and Designers",
+    org: "Fil-Am Association of Designers (FAAD)",
+    blurb: "A nonprofit organization founded by Lou Razon meant to represent creative FilipinX American artists and designers in the fashion industry. (Contact Info: (818-242-2088)",
+    categories: ["Labor"],
+    location: ["Glendale", "Southern California"],
+    language: ["English", "Tagalog"],
+    link: "[NONE]"
+    },
+    {
+    id: 4,
+    title: "Filipino American Nurses in SoCal",
+    org: "Philippine Nurses Association of Southern California (PNSAC, Inc.)",
+    blurb: "A professional nonprofit organization consists of Filipino American nurses in Southern California that promotes professional excellence and contributions to healthcare and society.",
+    categories: ["Labor"],
+    location: ["Southern California"],
+    language: ["English"],
+    link: "https://mypnasc.org/"
+    },
+    {
+    id: 4,
+    title: "Filipino American Lawyers of San Diego Promoting Services",
+    org: "Filipino American Lawyers of San Diego",
+    blurb: "A volunteer nonprofit organization dedicated to advancing Filipino American legal professionals who promote and provide educational, networking, and business opportunities for its members, along with volunteer events and legal clinics.",
+    categories: ["Labor", "Civic"],
+    location: ["San Diego", "Southern California"],
+    language: ["English"],
+    link: "https://falsd.org/"
+    },
+    {
+    id: 4,
+    title: "Filipino American Veterans Association in San Diego",
+    org: "Filipino American Veterans Association (FAVA)",
+    blurb: "A Filipino American veteran association that serves all American veterans from all branches of military service in San Diego's Filipino American community. (Contact info: (619)-232-2692)",
+    categories: ["Labor", "Civic"],
+    location: ["San Diego", "Southern California"],
+    language: ["English"],
+    link: "[NONE]"
+    },
+
+    // EDUCATION RESOURCES
+    {
+    id: 4,
+    title: "San Francisco After School and Summer Program for K-8 Filipino American Students",
+    org: "Filipino Education Center Galing Bata",
+    blurb: "A welcoming Filipino American bilingual/bicultural after school and summer program for K-8th students, educating Filipino American children in San Francisco for Filipino families of all backgrounds.",
+    categories: ["Education", "Civic"],
+    location: ["San Francisco", "Northern California"],
+    language: ["English", "Tagalog"],
+    link: "https://www.galingbata.org/"
+    },
+
+
+    // ARTS AND CULTURE
+    {
+    id: 5,
+    title: "Promoting FilAm Arts and Culture Through the Community",
+    org: "FilAm ARTS",
+    blurb: "Dedicated to promoting and empowering FilipinX American artists and culture bearers by sharing resources, providing platforms, and building community",
+    categories: ["Culture", "Education", "Civic"],
+    location: ["Los Angeles", "Southern California"],
+    language: ["English"],
+    link: "https://www.filamartsla.org/"
+    },
+    { // Media
+    id: 7,
+    title: "Filipino American One Down Media For Stories To Be Heard Online",
+    org: "One Down",
+    blurb: "A leading Filipino American media brand that creates content that resonates with Filipino Americans, providing a platform where their stores and experiences are seen and heard through social media. (Contact Info: partnerships@one-down.com",
+    categories: ["Culture", "Civic"],
+    location: ["[UNPUBLICIZED]"],
+    language: ["English"],
+    link: "https://onedown.media/"
+    },
+    { // Cultural Dance Groups
+    id: 7,
+    title: "Malaya Dance Group (Los Angeles)",
+    org: "Malaya Filipino American Dance Arts",
+    blurb: "A Filipino American ensemble of performing artists, dancers, and musicians who embrace ancestral and present culture through traditional folk dance, to inspire future generations through the Tagalog message of malaya (to be free).",
+    categories: ["Culture"],
+    location: ["Los Angeles", "Southern California"],
+    language: ["English", "Tagalog"],
+    link: "https://www.malayadance.org/"
+    },
+
+    // Traditional Martial Arts Groups (Kali/Arnis)
+    
+
+]
+
+
+
+  
